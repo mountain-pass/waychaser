7e5ff24e7f3218bf2a3c5f1f00a9df9288e6a9ca
Tom Howard <tom.howard@greater.com.au>
2020-10-28 22:40:33 +1100

    fixes for webpack upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = ['source-map'].map((devtool) => ({
   mode: 'development',
   entry: './src/waychaser.js',
-  //  entry: ['babel-polyfill', './src/waychaser.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'waychaser.js',
-    // chunkFilename: 'waychaser.js',
-    // publicPath: '/',
-    // devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    // devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]',
     libraryTarget: 'umd',
-    // library: 'waychaser',
   },
   devtool,
   optimization: {
@@ -31,12 +25,6 @@ module.exports = ['source-map'].map((devtool) => ({
   resolve: {
     extensions: ['.js'],
   },
-  // devServer: {
-  //   watchContentBase: true,
-  //   historyApiFallback: true,
-  //   port: 3000,
-  //   open: true,
-  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
