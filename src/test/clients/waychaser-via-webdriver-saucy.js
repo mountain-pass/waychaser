271fdd64458f4029b27801bbd92d1e96531eeebf
Tom Howard <tom.howard@greater.com.au>
2020-10-30 08:50:38 +1100

    Ignore local run parameters from code coverage

--- src/test/clients/waychaser-via-webdriver-saucy.js
+++ src/test/clients/waychaser-via-webdriver-saucy.js
@@ -14,8 +14,12 @@ const RUN =
   dateFormat(new Date(), "yyyy-MM-dd'T'hh:mm:ss.l ") +
   moment().tz(Intl.DateTimeFormat().resolvedOptions().timeZone).zoneAbbr();
 
-const BUILD = `${process.env.GITHUB_RUN_ID || 'LOCAL'}-${
-  process.env.GITHUB_RUN_NUMBER || RUN
+const BUILD = `${
+  process.env.GITHUB_RUN_ID ||
+  /* istanbul ignore next: only gets executed locally */ 'LOCAL'
+}-${
+  process.env.GITHUB_RUN_NUMBER ||
+  /* istanbul ignore next: only gets executed locally */ RUN
 }`;
 
 const TUNNEL_ID = `${BUILD}-${RUN}`;
