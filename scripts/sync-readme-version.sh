a4db7f809267ed5bcda8bfb0b671082d53a97ebd
Tom Howard <tom.howard@greater.com.au>
2020-11-09 12:21:32 +1100

    Fixed linting errors - added .sh linting to pre-commit hook

--- scripts/sync-readme-version.sh
+++ scripts/sync-readme-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
+: "${npm_package_name?This script should be run from npm}"
+: "${npm_package_version?This script should be run from npm}"
 
 sed -i -e "s|\"https://unpkg.com/${npm_package_name}@.*\"|\"https://unpkg.com/${npm_package_name}@${npm_package_version}\"|" README.md
