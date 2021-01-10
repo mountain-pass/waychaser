2ec6e46c65c636c14b5b6ecbe160e7e7c87da876
Tom Howard <tom.howard@greater.com.au>
2021-01-10 23:59:02 +1100

    test functions refactor - to make it easy to drill down in IDEs

--- src/test/resource.steps.js
+++ src/test/resource.steps.js
@@ -1,4 +1,4 @@
-import { Given, Before } from '@cucumber/cucumber'
+import { Given } from '@cucumber/cucumber'
 import LinkHeader from 'http-link-header'
 import { API_ACCESS_HOST, API_ACCESS_PORT } from './config'
 
@@ -18,7 +18,27 @@ const randomApiPath = () => {
   })}`
 }
 
+function createLinks (relationship, uri) {
+  const links = new LinkHeader()
+  links.set({
+    rel: relationship,
+    uri: uri
+  })
+  return links
+}
+
+function sendResponse (response, status, links, linkTemplates) {
+  if (links) {
+    response.header('link', links.toString())
+  }
+  if (linkTemplates) {
+    response.header('link-template', linkTemplates.toString())
+  }
+  response.status(status).send({ status })
+}
+
 async function createDynamicResourceRoute (
+  rootRouter,
   route,
   relationship,
   method,
@@ -47,7 +67,7 @@ async function createDynamicResourceRoute (
       })
       .join()}}`
   }
-  const dynamicRoute = await this.router.route(dynamicRoutePath)
+  const dynamicRoute = await rootRouter.route(dynamicRoutePath)
   await dynamicRoute[method.toLowerCase()](async (request, response) => {
     // logger.debug('SENDING', method, route, { ...request.query })
     logger.debug('RECEIVED BODY', method, route, { ...request.body })
@@ -90,81 +110,62 @@ async function createDynamicResourceRoute (
     })
   })
 
-  this.currentResourceRoute = randomApiPath()
-  await this.createRouteWithLinks(
-    this.currentResourceRoute,
+  const currentResourceRoute = randomApiPath()
+  await createRouteWithLinks(
+    rootRouter,
+    currentResourceRoute,
     200,
     undefined,
     links
   )
+  return currentResourceRoute
 }
 
-Before(async function () {
-  this.createRouteWithLinks = async function (
-    route,
-    status,
-    links,
-    linkTemplates
-  ) {
-    const router = await this.router.route(route)
-    await router.get(async (request, response) => {
-      this.sendResponse(response, status, links, linkTemplates)
-    })
-    return router
-  }
+async function createRouteWithLinks (
+  rootRouter,
+  route,
+  status,
+  links,
+  linkTemplates
+) {
+  const router = await rootRouter.route(route)
+  await router.get(async (request, response) => {
+    sendResponse(response, status, links, linkTemplates)
+  })
+  return router
+}
 
-  this.createRoute = async function (route, status, relationship, linkPath) {
-    if (relationship) {
-      const links = this.createLinks(relationship, linkPath)
-      return this.createRouteWithLinks(route, status, links)
-    } else {
-      return this.createRouteWithLinks(route, status)
-    }
+async function createRoute (rootRouter, route, status, relationship, linkPath) {
+  if (relationship) {
+    const links = createLinks(relationship, linkPath)
+    return createRouteWithLinks(rootRouter, route, status, links)
+  } else {
+    return createRouteWithLinks(rootRouter, route, status)
   }
-
-  this.createDynamicResourceRoute = createDynamicResourceRoute.bind(this)
-
-  this.createLinks = function (relationship, uri) {
-    const links = new LinkHeader()
-    links.set({
-      rel: relationship,
-      uri: uri
-    })
-    return links
-  }
-
-  this.sendResponse = function (response, status, links, linkTemplates) {
-    if (links) {
-      response.header('link', links.toString())
-    }
-    if (linkTemplates) {
-      response.header('link-template', linkTemplates.toString())
-    }
-    response.status(status).send({ status })
-  }
-})
+}
 
 Given('a resource returning status code {int}', async function (status) {
   this.currentResourceRoute = randomApiPath()
-  await this.createRoute(this.currentResourceRoute, status)
+  await createRoute(this.router, this.currentResourceRoute, status)
   this.firstResourceRoute = `http://${API_ACCESS_HOST}:${API_ACCESS_PORT}${this.currentResourceRoute}`
 })
 
 Given('a resource with no operations', async function () {
   this.currentResourceRoute = randomApiPath()
-  await this.createRouteWithLinks(this.currentResourceRoute, 200)
+  await createRouteWithLinks(this.router, this.currentResourceRoute, 200)
 })
 
 Given('a resource with a {string} operation', async function (relationship) {
   this.currentResourceRoute = randomApiPath()
-  await this.createRoute(this.currentResourceRoute, 200, relationship)
+  await createRoute(this.router, this.currentResourceRoute, 200, relationship)
 })
 
 Given(
   'a resource with a {string} operation that returns itself',
   async function (relationship) {
     this.currentResourceRoute = randomApiPath()
-    await this.createRoute(
+    await createRoute(
+      this.router,
       this.currentResourceRoute,
       200,
       relationship,
@@ -177,7 +178,8 @@ Given(
   'a resource with a {string} operation that returns an error',
   async function (relationship) {
     this.currentResourceRoute = randomApiPath()
-    await this.createRoute(
+    await createRoute(
+      this.router,
       this.currentResourceRoute,
       200,
       relationship,
@@ -191,7 +193,8 @@ Given(
   async function (relationship) {
     const previousResourceRoute = this.currentResourceRoute
     this.currentResourceRoute = randomApiPath()
-    await this.createRoute(
+    await createRoute(
+      this.router,
       this.currentResourceRoute,
       200,
       relationship,
@@ -205,13 +208,14 @@ Given(
   async function (count, relationship) {
     // we add the last one first
     this.currentResourceRoute = randomApiPath()
-    await this.createRoute(this.currentResourceRoute, 200)
+    await createRoute(this.router, this.currentResourceRoute, 200)
     this.lastOnList = `http://${API_ACCESS_HOST}:${API_ACCESS_PORT}${this.currentResourceRoute}`
     for (let index = 1; index < count; index++) {
       // and then point each on to the previously created resource
       const previousResourceRoute = this.currentResourceRoute
       this.currentResourceRoute = randomApiPath()
-      await this.createRoute(
+      await createRoute(
+        this.router,
         this.currentResourceRoute,
         200,
         relationship,
@@ -225,7 +229,8 @@ Given(
 Given(
   'a resource with a {string} operation that returns the provided {string} {string} parameter',
   async function (relationship, parameter, parameterType) {
-    await this.createDynamicResourceRoute(
+    this.currentResourceRoute = await createDynamicResourceRoute(
+      this.router,
       randomApiPath(),
       relationship,
       'GET',
@@ -245,14 +250,15 @@ Given(
       uri: this.currentResourceRoute,
       method
     })
-    await this.createRouteWithLinks(
+    await createRouteWithLinks(
+      this.router,
       this.currentResourceRoute,
       200,
       undefined,
       links
     ).then(route => {
       route[method.toLowerCase()](async (request, response) => {
-        this.sendResponse(response, statusCode)
+        sendResponse(response, statusCode)
       })
     })
   }
@@ -261,7 +267,8 @@ Given(
 Given(
   'a resource with a {string} operation with the {string} method that returns the provided {string} {string} parameter',
   async function (relationship, method, parameter, parameterType) {
-    await this.createDynamicResourceRoute(
+    this.currentResourceRoute = await createDynamicResourceRoute(
+      this.router,
       randomApiPath(),
       relationship,
       method,
@@ -274,7 +281,8 @@ Given(
 Given(
   'a resource with a {string} operation with the {string} method that returns the {string} provided {string} {string} parameter and the content type',
   async function (relationship, method, contentType, parameter, parameterType) {
-    await this.createDynamicResourceRoute(
+    this.currentResourceRoute = await createDynamicResourceRoute(
+      this.router,
       randomApiPath(),
       relationship,
       method,
@@ -294,7 +302,8 @@ Given(
     parameterType,
     contentTypes
   ) {
-    await this.createDynamicResourceRoute(
+    this.currentResourceRoute = await createDynamicResourceRoute(
+      this.router,
       randomApiPath(),
       relationship,
       method,
@@ -309,7 +318,8 @@ Given(
   'a resource with a {string} operation with the {string} method that returns the following provided parameters',
   async function (relationship, method, dataTable) {
     const parameters = dataTable.hashes()
-    await this.createDynamicResourceRoute(
+    this.currentResourceRoute = await createDynamicResourceRoute(
+      this.router,
       randomApiPath(),
       relationship,
       method,
