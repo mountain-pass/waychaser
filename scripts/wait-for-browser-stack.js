12e57657a5678ec5709297a7e982fde1b731fed9
Tom Howard <tom.howard@greater.com.au>
2021-01-12 01:42:30 +1100

    ci(browserstack): increased random wait before browserstack availability polling

--- scripts/wait-for-browser-stack.js
+++ scripts/wait-for-browser-stack.js
@@ -51,17 +51,17 @@ const MAX_WAIT = 10 * 60 * 60 // 10hr
 const RESERVED_SESSIONS = 1
 const MIN_WAIT_TIME = 40
 const MAX_WAIT_TIME = 80
-const MAX_INIT_WAIT_TIME = 20
+const MAX_INIT_WAIT_TIME = 120
 /**
  *
  */
 async function waitForSpareSession () {
   const start = Date.now()
+  // inital random sleep so all the different browserstack tests don't try to start at once
+  const initialWaitTime = getRandomInt(0, MAX_INIT_WAIT_TIME)
+  logger.info(`waiting ${initialWaitTime} before starting availability check`)
   while (secondsSince(start) < MAX_WAIT) {
-    // inital random sleep so all the different browserstack tests don't try to start at once
-    await new Promise(resolve =>
-      setTimeout(resolve, getRandomInt(0, MAX_INIT_WAIT_TIME) * 1000)
-    )
+    await new Promise(resolve => setTimeout(resolve, initialWaitTime * 1000))
     const planInfo = await getPlanInfo()
     const maxAllowed =
       planInfo.parallel_sessions_max_allowed - RESERVED_SESSIONS
