93932b8d6f40afaece4e9ffd2af80ce17d79e94e
Tom Howard <tom.howard@greater.com.au>
2021-01-13 15:24:10 +1100

    refactor(test): moved utility functions

--- src/test/load-resource.steps.js
+++ src/test/load-resource.steps.js
@@ -1,31 +1,40 @@
 import { expect } from 'chai'
-import { When, Then, Before } from '@cucumber/cucumber'
+import { When, Then } from '@cucumber/cucumber'
 import logger from '../util/logger'
 import { API_ACCESS_PORT, API_ACCESS_HOST } from './config'
 
-Before(async function () {
-  this.loadCurrentResource = async function () {
-    return this.loadResourceByPath(this.currentResourceRoute)
-  }
+async function loadResourceByUrl (waychaserProxy, url) {
+  logger.debug(`loading ${url}`)
+  return waychaserProxy.load(url)
+}
 
-  this.loadResourceByPath = async function (path) {
-    return this.loadResourceByUrl(
-      `http://${API_ACCESS_HOST}:${API_ACCESS_PORT}${path}`
-    )
-  }
+async function loadResourceByPath (waychaserProxy, path) {
+  return loadResourceByUrl(
+    waychaserProxy,
+    `http://${API_ACCESS_HOST}:${API_ACCESS_PORT}${path}`
+  )
+}
 
-  this.loadResourceByUrl = async function (url) {
-    logger.debug(`loading ${url}`)
-    this.result = await this.waychaserProxy.load(url)
-  }
-})
+async function loadAndCheckResourceByPath (
+  waychaserProxy,
+  currentResourceRoute
+) {
+  const result = await loadResourceByPath(waychaserProxy, currentResourceRoute)
+  expect(result.success).to.be.true()
+  return result
+}
 
 When('waychaser loads that resource', async function () {
-  await this.loadCurrentResource()
+  this.result = await loadResourceByPath(
+    this.waychaserProxy,
+    this.currentResourceRoute
+  )
 })
 
 When("waychaser loads a resource that's not available", async function () {
-  await this.loadResourceByUrl(`http://${API_ACCESS_HOST}:33556/api`)
+  this.result = await loadResourceByUrl(this.waychaserProxy,
+    `http://${API_ACCESS_HOST}:33556/api`
+  )
 })
 
 Then('it will have loaded successfully', async function () {
@@ -37,19 +46,25 @@ Then('it will NOT have loaded successfully', async function () {
 })
 
 When('waychaser successfully loads that resource', async function () {
-  await this.loadCurrentResource()
-  expect(this.result.success).to.be.true()
+  this.result = await loadAndCheckResourceByPath(
+    this.waychaserProxy,
+    this.currentResourceRoute
+  )
 })
 
 When('waychaser successfully loads the latter resource', async function () {
-  await this.loadCurrentResource()
-  expect(this.result.success).to.be.true()
+  this.result = await loadAndCheckResourceByPath(
+    this.waychaserProxy,
+    this.currentResourceRoute
+  )
 })
 
 When(
   'waychaser successfully loads the first resource in the list',
   async function () {
-    await this.loadCurrentResource()
-    expect(this.result.success).to.be.true()
+    this.result = await loadAndCheckResourceByPath(
+      this.waychaserProxy,
+      this.currentResourceRoute
+    )
   }
 )
