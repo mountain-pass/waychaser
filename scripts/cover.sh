b63acfeb5d23a76d77898ea7a5b8d6c9809f8972
Tom Howard <tom.howard@greater.com.au>
2021-02-15 16:33:11 +1100

    fix(lint): fixing linting issue

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -7,6 +7,7 @@ COVERING=${npm_lifecycle_event#cover:}
 COVERING_WITHOUT_SKIPABLE=${COVERING//:not\[@skippable\]/}
 COVER_NAME=${COVERING//[:@\[\]]/-}
 COVER_NAME_WITHOUT_SKIPABLE=${COVERING_WITHOUT_SKIPABLE//[:@\[\]]/-}
+# shellcheck disable=SC2001
 COVER_NAME_ALL=$(echo "${COVER_NAME_WITHOUT_SKIPABLE}" | sed 's/browser-api-[^-]*-/browser-api-/')
 
 COVERAGE=1 nyc \
