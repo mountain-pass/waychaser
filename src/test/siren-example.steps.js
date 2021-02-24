0f2545097afad5efd8c3ba9599917115438a2b63
Tom Howard <tom.howard@greater.com.au>
2021-02-24 23:15:54 +1100

    fix(lint): remove unused import

--- src/test/siren-example.steps.js
+++ src/test/siren-example.steps.js
@@ -1,7 +1,6 @@
 import { Given, Then } from '@cucumber/cucumber'
 import { expect } from 'chai'
 import fetch from 'isomorphic-fetch'
-import logger from '../util/logger'
 import { handleResponseError } from './handle-response-error'
 
 Given('Assuming a Siren API is available at {string}', async function (url) {
