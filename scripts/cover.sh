daffdf041e63674b0388bc939cf7fe98dab2c97e
Tom Howard <tom@windyroad.com.au>
2021-11-15 13:09:39 +1100

    fix(test): fixed test run errors in zsh
    zsh didn't like '[' and ']' in the npm script name

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -4,9 +4,9 @@
 
 # need to remove `:not[@skippable]` if it's present
 COVERING=${npm_lifecycle_event#cover:}
-COVERING_WITHOUT_SKIPABLE=${COVERING//:not\[@skippable\]/}
-COVER_NAME=${COVERING//[:@\[\]]/-}
-COVER_NAME_WITHOUT_SKIPABLE=${COVERING_WITHOUT_SKIPABLE//[:@\[\]]/-}
+COVERING_WITHOUT_SKIPABLE=${COVERING//:not\{@skippable\}/}
+COVER_NAME=${COVERING//[:@\{\}]/-}
+COVER_NAME_WITHOUT_SKIPABLE=${COVERING_WITHOUT_SKIPABLE//[:@\{\}]/-}
 # shellcheck disable=SC2001
 COVER_NAME_ALL=$(echo "${COVER_NAME_WITHOUT_SKIPABLE}" | sed 's/browser-api-[^-]*-/browser-api-/')
 
